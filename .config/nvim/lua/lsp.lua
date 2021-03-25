041a02f953ff8a9e42040dac4316dab49220fb9d
João Fraga <joaoluisfreirefraga@gmail.com>
2021-03-25 20:40:55 +0000

    Update editor configuration

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -47,7 +47,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {"clangd", "ocamllsp", "pyright", "texlab" }
+local servers = {"clangd", "pyright", "ocamllsp", "hls", "rust_analyzer", "tsserver", "texlab" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
