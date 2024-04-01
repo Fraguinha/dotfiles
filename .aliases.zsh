4c61599a8e98462ea9af74d76a7659a754d1fe9d
João Fraga <joao@fraguinha.com>
2024-04-01 07:06:47 +0100

    Update configurations

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,46 +1,14 @@
-alias mkdir='mkdir -p'
-
 alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
 
-alias l='dirs -v'
-
-alias l0='cd -0'
-alias l1='cd -1'
-alias l2='cd -2'
-alias l3='cd -3'
-alias l4='cd -4'
-alias l5='cd -5'
-alias l6='cd -6'
-alias l7='cd -7'
-alias l8='cd -8'
-alias l9='cd -9'
-
-alias ls='ls -Gv'
+alias ls='ls -v -G --color=auto'
 alias ll='ls -lh'
 alias la='ls -lhA'
 alias l.='ls -lhd .*'
 
-alias .2='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-alias .6='cd ../../../../../..'
-alias .7='cd ../../../../../../..'
-alias .8='cd ../../../../../../../..'
-alias .9='cd ../../../../../../../../..'
-
-alias f='open -a Finder .'
-
-alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
-alias python='/usr/local/bin/python3'
-alias pip='/usr/local/bin/pip3'
-
-alias ubuntu='docker run --rm -it -v "$PWD":"/mnt/$PWD" -w "/mnt/$PWD" ubuntu:latest'
-alias ubuntu32='docker run --rm -it -v "$PWD":"/mnt/$PWD" -w "/mnt/$PWD" i386/ubuntu:latest'
+alias dotfiles='git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
 
 alias tree='tree -a -I ".git"'
 
 alias code='code -r'
+
+alias f='open -a Finder .'
--- .zshrc
+++ .zshrc
@@ -10,36 +10,5 @@ source "$HOME/.variables.zsh"
 # ocaml
 eval $(opam env)
 
-# rust
-source "$HOME/.cargo/env"
-
-# sdkman
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# syntax highlighting
-source /usr/local/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-
-# auto complete
-source /usr/local/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-
-bindkey '\t' menu-select "$terminfo[kcbt]" menu-select
-bindkey -M menuselect '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete
-
-zstyle ':autocomplete:*complete*:*' insert-unambiguous yes
-zstyle ':autocomplete:*history*:*' insert-unambiguous yes
-zstyle ':autocomplete:menu-search:*' insert-unambiguous yes
-
-# auto suggestions
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# auto pair
-source /usr/local/share/zsh-autopair/autopair.zsh
-
 # starship
 eval "$(starship init zsh)"
