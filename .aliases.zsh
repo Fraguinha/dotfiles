51bedc21b41c3cefabf8a952563bf7f8bbde2fa7
João Fraga <joao@fraguinha.com>
2024-10-05 22:18:54 +0100

    feat: Configure dotfiles

---
+++ .aliases.zsh
@@ -0,0 +1,15 @@
+alias grep='grep --color=auto'
+
+alias ls='ls -v -G --color=auto'
+alias ll='ls -lh'
+alias la='ls -lhA'
+alias l.='ls -lhd .*'
+
+alias dotfiles='git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
+
+alias tree='tree -a -I ".git"'
+
+alias code='code -r'
+alias vim='nvim'
+
+alias f='open -a Finder .'
