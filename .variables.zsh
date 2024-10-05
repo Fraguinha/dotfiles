bf45503b05b4e1dece73f2e482f31e4da6043679
João Fraga <joao@fraguinha.com>
2024-10-05 22:18:54 +0100

    Clear history

---
+++ .variables.zsh
@@ -0,0 +1,6 @@
+# Workspace
+export WORKSPACE="$HOME/workspace"
+
+# iCloud
+icloud="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
+home="$HOME/Library/Mobile Documents/com~apple~CloudDocs/home"
